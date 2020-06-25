982b7d56e98a2a6bb97719ae7e1e9ff532cbd3bb
Jan Krupa <jankrupa93@gmail.com>
2020-06-25 10:59:37 +0200

    Add files for homepage

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import './App.css';
 
+import HomePage from './homepage.component'
+
 function App() {
-  return <div> Hello </div>;
+  return <div> 
+    <div>
+      <HomePage></HomePage>
+    </div>
+
+  </div>;
 }
 
 export default App;
